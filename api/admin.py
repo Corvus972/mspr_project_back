8af1a83a7c68be1663e084a63dfba55796c47c0a
stanleyHarry <jesoph.stanley@hotmail.com>
2021-03-02 14:57:42 +0100

    add salesrulemodel

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from api.models.product import Product
+from api.models.sales_rule import SalesRule
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from api.resource import *
@@ -30,3 +31,33 @@ class ProductAdmin(ImportExportModelAdmin, admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
+
+
+@admin.register(SalesRule)
+class SalesRuleAdmin(admin.ModelAdmin):
+    # GET
+    list_display = ('name', 'description',
+                    'from_data', 'to_data', 'coupon_code', 'discount_amount', 'product_associated')
+
+    # POST
+    fieldsets = (
+        ('Informations requises', {
+            'fields': ('name', 'description', 'from_data', 'to_data', 'coupon_code', 'discount_amount', 'product_associated')
+        }),
+    )
+    # get product name to associate it to the coupon
+
+    def product_associated(self, obj):
+        return obj.product_associated
+
+    #product_associated.empty_value_display = 'api.Product.None'
+
+    def get_queryset(self, request):
+        # for super admin
+        if request.user.is_authenticated and request.user.is_superuser:
+            qs = super(SalesRuleAdmin, self).get_queryset(request)
+            return qs
+        else:
+            # for admin
+            qs = super(ProductAdmin, self).get_queryset(request)
+            return qs.filter(SalesRuleAdmin=request.user)
---
+++ api/models/sales_rule.py
@@ -0,0 +1,18 @@
+from django.db import models
+from api.models.product import Product
+
+
+class SalesRule(models.Model):
+    name = models.CharField(
+        max_length=30, verbose_name="Nom du coupon")
+    description = models.CharField(max_length=500)
+    from_data = models.DateField(verbose_name="Date de début")
+    to_data = models.DateField(verbose_name="Date de fin")
+    coupon_code = models.CharField(
+        max_length=30, verbose_name="Code du coupon")
+    discount_amount = models.IntegerField(default=0, verbose_name="Remise")
+    product_associated = models.ManyToManyField(
+        Product, verbose_name="produits")
+
+    def __str__(self):
+        return self.name
